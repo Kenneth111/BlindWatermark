3f42a805bfc60f4b2050f68c801713bcc30da7ff
guokun <guokun@vanxum.com>
2018-12-17 17:02:29 +0800

    move zigzag to utils.py

--- water_dct.py
+++ water_dct.py
@@ -1,31 +1,7 @@
 import numpy as np
 from scipy.fftpack import dct, idct
 from PIL import Image
-
-def zigzag(N):
-    try:
-        zz_mtx = np.zeros((N,N))
-    except TypeError as err:
-        print("N must be an integer, error msg: {0}".format(err))
-        return -1
-    except ValueError as err:
-        print("N must be larger than 0, error msg: {0}".format(err))
-        return -1
-    for n in range(2,N+2):
-        zz_mtx[n-2, 0] = 1/2*((-1)**n)*(n+((-1)**n)*((n-2)*n+2)-2)
-    for i in range(1, N+1):
-        for j in range(2, N+1):
-            if ((i % 2 == 0 and j % 2 == 0) or (i % 2 == 1 and j % 2 == 1)):
-                zz_mtx[i-1, j-1] = zz_mtx[i-1, j-2] + (2*i-1)
-            else:
-                zz_mtx[i-1, j-1] = zz_mtx[i-1, j-2] + (j-1)*2
-    zz_mtx = np.fliplr(zz_mtx)
-    for i in range(2, N+1):
-        for j in range(1, N):
-            if i > j:
-                zz_mtx[i-1, j-1] = zz_mtx[i-1, j-1] - (i - j) ** 2
-    zz_mtx = np.fliplr(zz_mtx)    
-    return zz_mtx
+from utils import zigzag
 
 lena = Image.open("lena_gray.png")
 lena = lena.convert("L")
